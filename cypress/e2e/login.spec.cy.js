907eabaea2fbe0a4c352220ea1173f6ba971696b
Felipe Barillo <felipesantanabarillo@gmail.com>
2025-12-08 14:18:10 -0300

    Refactor login tests to use selectorsList

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,23 +1,30 @@
-
-
 describe('ORANGE', () => {
+
+  const selectorsList = {
+    usernameFiel: "[name='username']",
+    userpasswordFiel: "[name='password']",
+    loginButton: "button",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    wrongCredentialAlert: ".oxd-alert"
+    }
+  
   it('Login - Sucesso', () => { //it.skip ele pula este teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get('button').click()
+    cy.get(selectorsList.usernameFiel).type('Admin')
+    cy.get(selectorsList.userpasswordFiel).type('admin123')
+    cy.get(selectorsList.loginButton).click()
      //location = Saber qual URL está / should = igualar o endereço
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     // contains = Confirma elemento na pagina logada!!!
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(sectionTitleTopBar).contains('Dashboard')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test')
-    cy.get("[name='password']").type('test')
-    cy.get('button').click()
-    cy.get('.oxd-alert')
+    cy.get(selectorsList.usernameFiel).type('test')
+    cy.get(selectorsList.userpasswordFiel).type('test')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsListwrongCredentialAlert)
     
   })
 
