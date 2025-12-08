4063e3c071717aa9f27b3488effde5a313371ef1
FelipeBarillo <felipe@mintQA.felipe>
2025-12-08 15:09:16 -0300

    Testandogit add .!

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -10,16 +10,16 @@ describe('ORANGE', () => {
     wrongCredentialAlert: ".oxd-alert"
   }
 
-  
   it('Login - Sucesso', () => { //it.skip ele pula este teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameFiel).type(userData.userSucess.userName)
     cy.get(selectorsList.userpasswordFiel).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
-     //location = Saber qual URL está / should = igualar o endereço
+    //location = Saber qual URL está / should = igualar o endereço
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     // contains = Confirma elemento na pagina logada!!!
-    cy.get(selectorsList.sectionTitleTopBar)
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+
   })
 
   it('Login - Fail', () => {
@@ -28,7 +28,9 @@ describe('ORANGE', () => {
     cy.get(selectorsList.userpasswordFiel).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert) 
+
   })
 
 
 })
+
